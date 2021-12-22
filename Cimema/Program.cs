b97d1f0806d6653edfe77900881e7d28a045d48c
Omaenoshi <rassvetalov.maksim@mail.ru>
2021-12-23 00:56:19 +0500

    Initial commit

---
+++ Cimema/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cimema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
